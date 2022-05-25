368dc643e860009a50ec6ebec3bf3981256aaee5
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2022-05-25 12:36:24 +0900

    학습용 이미지 keypoint feature로 변환 및 min feature로 정규화

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from objectDetection import *
 if __name__ =='__main__':
     category = "ant"
     detector = object_detector(category=category)
-    for i in detector.train_data:
-        detector.SIFT(img=i)
-    detector.show_image()
+    detector.detector()
+    print(len(detector.train_data))
+    print(len(detector.test_data))
+
--- objectDetection.py
+++ objectDetection.py
@@ -1,3 +1,4 @@
+from cv2 import KeyPoint_convert
 import numpy as np
 try:
     from cv2 import cv2
@@ -8,22 +9,33 @@ from fileRead import file_read
 class object_detector:
     def __init__(self, category):
         self.category = category
-        self.train_data, self.test_data = file_read(category)
+        self.train_img, self.test_img = file_read(category)
+        self.train_data, self.test_data = [],[]
 
     def show_image(self):
-        cv2.imshow("test",self.train_data[1])
+        cv2.imshow("test",self.train_img[1])
         cv2.waitKey(0)
+
     def detector(self):
-        
         self.SIFT()
 
-    def SIFT(self, img):
+    def SIFT(self):
         # TODO 이미지 전체의 특징점 최소값 추출후 nfeatures 설정
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # min feature
+        min = 10000
+        for i in self.train_data + self.train_data:
+            gray = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+            sift = cv2.xfeatures2d.SIFT_create()
+            keypoint = sift.detect(gray)
+            min = len(keypoint) if len(keypoint) < min else min
+        
+        sift = cv2.xfeatures2d.SIFT_create(nfeatures = min)
+        for img in self.train_img:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            keypoint = sift.detect(gray)
+            self.train_data.append(KeyPoint_convert(keypoint))
 
-        sift = cv2.xfeatures2d.SIFT_create(nfeatures = 100)
-        keypoint = sift.detect(gray)
-        cv2.drawKeypoints(img, keypoint, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # cv2.imshow("SIFT features", img)
-        print(len(keypoint))
-        return keypoint
+        for img in self.test_img:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            keypoint = sift.detect(gray)
+            self.test_data.append(KeyPoint_convert(keypoint))
