316ecca3531bc37ad636a02a0e75e40b3d5400bf
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2022-05-30 21:40:57 +0900

    exam upload

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ if __name__ =='__main__':
     category = "ant"
     detector = object_detector(category=category)
     detector.detector()
-    print(len(detector.train_data))
-    print(len(detector.test_data))
 
